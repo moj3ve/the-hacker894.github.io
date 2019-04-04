43011988c91141393318dce468d8c9c015da0691
The-Hacker894 <skylarmccauley894@gmail.com>
2019-04-04 14:46:57 -0400

    New version detector for Hacked Repo

--- hacked-repo/depic/js/index.js
+++ hacked-repo/depic/js/index.js
@@ -1,17 +1,16 @@
 (function(a){"use strict";navigator.userAgent.indexOf("Cydia")!=-1?(a.title=a.title.split(" \u00b7 ")[0],a.documentElement.classList.add("cydia")):a.documentElement.classList.remove("cydia","depiction")})(document)
 
 function iOSVersion() {
-    var match = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/),
-        version;
-    console.log(navigator.appVersion)
-    console.log(match)
-    if (match !== undefined && match !== null) {
-        version = [
-            parseInt(match[1], 10),
-            parseInt(match[2], 10),
-            parseInt(match[3] || 0, 10)
-        ];
-        return `<p>You are running iOS <strong>${parseFloat(version.join('.'))}</strong><p>`
+
+    var iOSVer = navigator.appVersion.match(/CPU( iPhone)? OS (\d+)_(\d+)(_(\d+))? like/i);
+    if(!iOSVer) {
+        return `<p style="color: red;">iOS version not detected<p>`;
     }
-    return `<p style="color: red;">iOS version not detected<p>`;
+    var osVersion = [ iOSVer[2], iOSVer[3], iOSVer[4] ? iOSVer[5] : 0 ]
+    var osString = osVersion[0] + "." + osVersion[1] + (osVersion[2] && osVersion[2] != 0 ? "." + osVersion[2] : "")
+
+    
+        return `<p>You are running iOS <strong>${osString}</strong><p>`
+    
+    
 }
