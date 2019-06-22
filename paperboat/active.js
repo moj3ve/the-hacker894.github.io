ba29ee981ed29a4928c5b1ff59cc9c652d3a6d6d
TheHacker894 <skylarmccauley894@gmail.com>
2019-06-21 20:29:45 -0400

    Made some changes
    - 50 instead of 50vw for the input size
    - window.location.hash check is now performed _after_ the site loads instead of putting the browser into an infinite loop of loading

---
+++ paperboat/active.js
@@ -0,0 +1,5 @@
+setInterval( () => {
+    if(window.location.hash.length > 1) {
+        window.location.href="https://skylarmccauley.xyz/paperboat"
+    }
+}, 1000)
--- paperboat/main.js
+++ paperboat/main.js
@@ -6,9 +6,3 @@ function process() {
     // url=url.substring(0, url.length - 10); Use this when debugging locally
     window.location.href=`${url}msg/index.html#${msg}`
 }
-
-setInterval( () => {
-    if(window.location.hash.length > 1) {
-        window.location.href="https://skylarmccauley.xyz/paperboat"
-    }
-}, 1000)
