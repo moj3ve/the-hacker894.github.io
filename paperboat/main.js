8ce93547ba8c2e6f8c6d338bcb68637b01db9094
TheHacker894 <skylarmccauley894@gmail.com>
2019-06-21 20:35:49 -0400

    Final changes for the night

--- paperboat/main.js
+++ paperboat/main.js
@@ -3,6 +3,8 @@ function process() {
     msg = btoa(msg);
     
     url=window.location.href
-    // url=url.substring(0, url.length - 10); Use this when debugging locally
+    /* url=url.substring(0, url.length - 10); Use this when debugging locally
     window.location.href=`${url}msg/index.html#${msg}`
+    */
+   window.location.href=`${url}msg/index.html#${msg}`
 }
