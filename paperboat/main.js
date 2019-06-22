6879d7236a353aeffa816934f3daa2658d721da7
TheHacker894 <skylarmccauley894@gmail.com>
2019-06-21 20:23:26 -0400

    Forgot something

--- paperboat/main.js
+++ paperboat/main.js
@@ -3,7 +3,7 @@ function process() {
     msg = btoa(msg);
     
     url=window.location.href
-    url=url.substring(0, url.length - 10);
+    // url=url.substring(0, url.length - 10); Use this when debugging locally
     window.location.href=`${url}msg/index.html#${msg}`
 }
 
